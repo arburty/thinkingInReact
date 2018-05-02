afb82a29df0f7a89f0cbce2e6c0bd53846563edc
arburty <austin@burt.us.com>
2018-05-01 22:04:58 -0600

    solution to step 2-3
    I heavily copied from the answer as I was not going to figure this out
    on my own.  I still hand wrote all of it and much better understand how
    to go about adding these components to build a table.

--- solution.js
+++ solution.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const FilterableProductTable = React.createClass({
     render() {
+        const products = this.props.products;
+
         return (
-            <SearchBar/>
+            <div>
+                <SearchBar/>
+                <ProductTable products={products}/>
+            </div>
 
         )
 
@@ -20,6 +25,71 @@ export const SearchBar = React.createClass({
                     Only show products in stock
                 </label>
             </form>
+        )
+    }
+})
+export const ProductTable = React.createClass({
+    render() {
+        const products = this.props.products;
+
+        const rows = [];
+        let currentCategory;
+
+        products.forEach((item) => {
+            if (item.category !== currentCategory) {
+                currentCategory = item.category;
+
+                rows.push((
+                    <ProductCategoryRow
+                        key={currentCategory}
+                        category={currentCategory}
+                    />
+                ))
+            }
+
+            rows.push((
+                <ProductRow key={item.name} product={item}/>
+            ))
+        })
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        )
+
+    }
+})
+export const ProductCategoryRow = React.createClass({
+    render() {
+        return (
+            <tr>
+                <th colSpan={2}>{this.props.category}</th>
+            </tr>
+        )
+    }
+})
+export const ProductRow = React.createClass({
+    render() {
+        const product = this.props.product;
+
+        const style = {
+            color: product.stocked ? null : 'red',
+        };
+
+        return (
+            <tr>
+                <td style={style}>{product.name}</td>
+                <td>{product.price}</td>
+            </tr>
         )
     }
 })
